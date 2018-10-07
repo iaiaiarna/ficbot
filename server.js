7e4adfbfb99ea5412ac9daf208ccb3212495ce20
Rebecca Turner <me@re-becca.org>
2018-10-06 21:49:52 -0700

    General code cleanup

--- server.js
+++ server.js
@@ -23,23 +23,150 @@ async function main (opts, conffile) {
   conf = Object.assign({}, DEFAULTS, TOML.parse(fs.readFileSync(conffile)))
 
   client = new Discord.Client()
-    .on('ready',  eventRun(clientReady))
-    .on('error',  eventRun(clientError))
-    .on('message',  eventRun(clientMessage))
-    .on('messageReactionAdd',  eventRun(clientMessageReactionAdd))
-    .on('guildMemberAdd',  eventRun(clientGuildMemberAdd))
+    .on('ready',  asyncHandler(clientReady))
+    .on('error',  asyncHandler(clientError))
+    .on('message',  asyncHandler(clientMessage))
+    .on('messageReactionAdd',  asyncHandler(clientMessageReactionAdd))
+    .on('guildMemberAdd',  asyncHandler(clientGuildMemberAdd))
 
   client.login(conf.token)
 }
 
 // this little wrapper exists so that our async event handlers don't throw
 // away errors
-function eventRun (fn) {
+function asyncHandler (fn) {
   return async (...args) => {
     try {
       return await fn(...args)
     } catch (ex) {
       console.log(`Error running ${fn.name}:`, ex)
+    }
+  }
+}
+
+// Chatbot commands handled by clientMessage
+const Commands = {
+  'ping': {
+    usage: 'ping [args...]',
+    description: 'check to see if the bot is alive',
+    action: async ($, {args})  => {
+      return $.msg.reply(`I am alive`
+        + ($.server ? `\nYour server is ${name($.server)}` : '')
+        + (args ? '\nYour args were: ' + args.join('/') : ''))
+    }
+  },
+  'report': {
+    usage: 'report <reason...>',
+    description: 'report inappropriate activity',
+    filter: $ => $.server,
+    action: async ($, {reason}) => {
+      const chname = ($.msg.channel && $.msg.channel.type !== 'dm') ? $.msg.channel : 'DM'
+      console.log(`**REPORT** from ${name($.msg.author)} in ${name(chname)}: ${reason.join(' ')}`)
+      if ($.server.deleteReports && $.msg.channel.name) $.msg.delete().catch(() => {})
+      await $.server.moderation.send(`@here **REPORT** from ${$.msg.author} in ${chname}: ${reason.join(' ')}`)
+      return sendDM($.msg.author, `Report in ${chname} has been sent to moderators: ${reason.join(' ')}`).catch(() => {})
+    }
+  },
+  'admin': {
+    description: 'message the admins',
+    usage: 'admin <message...>',
+    filter: $ => $.server,
+    action: async ($, {message}) => {
+      const chname = ($.msg.channel && $.msg.channel.type !== 'dm') ? $.msg.channel : 'DM'
+      console.log(`Meessag eto admins from ${name($.msg.author)} in ${name(chname)}: ${message.join(' ')}`)
+      if ($.server.deleteReports && $.msg.channel.name) $.msg.delete().catch(() => {})
+      await $.server.moderation.send(`Admin message from ${$.msg.author} in ${chname}: ${message.join(' ')}`)
+      return sendDM($.msg.author, `Message to admins sent from ${chname}: ${message.join(' ')}`).catch(() => {})
+    }
+  }
+}
+
+// Client event handlers
+
+async function clientError (err) {
+  console.log(err)
+}
+
+async function clientGuildMemberAdd (gm) {
+  const server = conf.serversById[gm.guild.id]
+  return server.welcome.send(`Welcome to the server ${gm.user}! We are _so_ happy to see you! :heartpulse: Please check out the pinned message in this channel for the why's and whatfors of this server, plus our code of conduct!`)
+}
+
+async function clientMessageReactionAdd (mr, user) {
+  if (!mr.message.guild) return // dm
+  const server = conf.serversById[mr.message.guild.id]
+
+  const report = `Reporting ${mr.message.author} saying “${mr.message}”`
+  console.log(`**EMOJI REPORT** from ${name(user)} in ${name(mr.message.channel)}: ${report}`)
+  await mr.remove(user)
+  await server.moderation.send(`@here **EMOJI REPORT** from ${user} in ${mr.message.channel}: ${report}`)
+  return sendDM(user, `Report in ${mr.message.channel} has been sent to moderators: ${report}`).catch(() => {})
+}
+
+async function clientMessage (msg) {
+  if (msg.author.id === client.user.id) return // ignore our own messages
+  // Helpful when debugging, but logs ALL messages on the discord
+  //console.log('clientMessage', msg.channel.name, msg.author.id, msg.author.username, msg.author.discriminator, msg.content)
+  let cmdline = msg.content.trim()
+  if (qr`^/\w`.test(cmdline)) {
+    cmdline = cmdline.slice(1)
+  }
+
+  let server
+  if (msg.guild) {
+    server = conf.serversById[msg.guild.id]
+  } else {
+    // the msg is a DM, so we have find the user's server on our own, this
+    // is why we can't have nice things re: having this bot on multiple
+    // servers
+    const guilds = []
+    for (let [, guild] of client.guilds) {
+      try {
+        await guild.fetchMember(msg.author, true)
+        guilds.push(guild)
+      } catch (ex) {
+      }
+    }
+    if (guilds.length === 1) {
+      server = conf.serversById[guilds[0].id]
+    }
+  }
+  if (!server) {
+    await sendDM(msg.author, 'We were unable to determine exactly one server associated with you and this bot, the DM interface will be limited.')
+  }
+
+  // The callback defined on each `.command` will be run if appropriate by
+  // yargs.
+  let output
+  const yargs = require('yargs')()
+    .scriptName('')
+    .usage('')
+    .wrap(null)
+    .exitProcess(false)
+    .hide('version')
+    .hide('help')
+  Object.keys(Commands).forEach(name => {
+    const cmd = Commands[name]
+    if (!cmd.filter || cmd.filter({msg, server})) {
+      yargs.command(cmd.usage || name, cmd.description)
+    }
+  })
+  if (msg.channel.type === 'dm') yargs
+    .demand(1)
+    .recommendCommands()
+
+  // yargs.parse is how we get yargs to read the string we have instead of
+  // our actual commandline. output handling for yargs help is… silly.
+  yargs.parse(cmdline, (_1,_2,_3) => output = _3)
+  if (output) return msg.reply(output)
+  let argv = yargs.argv
+  if (argv) {
+    const cmd = Commands[argv._[0]]
+    if (cmd) {
+      return cmd.action({msg, server}, argv)
+    } else if (msg.channel.type === 'dm') {
+      yargs.showHelp((..._) => output = _)
+      return msg.reply(output.join(' '))
     }
   }
 }
@@ -73,111 +200,17 @@ async function clientReady () {
   })
 }
 
-async function clientError (err) {
-  console.log(err)
-}
-
-async function clientMessageReactionAdd (mr, user) {
-  if (!mr.message.guild) return // dm
-  const server = conf.serversById[mr.message.guild.id]
-
-  const report = `Reporting ${mr.message.author} saying “${mr.message}”`
-  console.log(`**EMOJI REPORT** from ${name(user)} in ${name(mr.message.channel) || 'DM'}: ${report}`)
-  await mr.remove(user)
-  await server.moderation.send(`@here **EMOJI REPORT** from ${user} in ${mr.message.channel || 'DM'}: ${report}`)
-  const dm = user.dmChannel || await user.createDM()
-  dm.send(`Report in ${mr.message.channel || 'DM'} has been received: ${report}`).catch(() => {})
-}
-
-async function sendDM (user, msg) {
-  const dm = user.dmChannel || user.createDM()
-  dm.send(msg)
-}
-
-async function clientMessage (msg) {
-  if (msg.author.id === client.user.id) return // ignore our own messages
-  // Helpful when debugging, but logs ALL messages on the discord
-  //console.log('clientMessage', msg.channel.name, msg.author.id, msg.author.username, msg.author.discriminator, msg.content)
-  let cmd = msg.content.trim()
-  if (qr`^/\w`.test(cmd)) {
-    cmd = cmd.slice(1)
-  }
-
-  let server
-  if (msg.guild) {
-    server = conf.serversById[msg.guild.id]
-  } else {
-    // the msg is a DM, so we have find the user's server on our own, this
-    // is why we can't have nice things re: having this bot on multiple
-    // servers
-    const guilds = []
-    for (let [, guild] of client.guilds) {
-      try {
-        await guild.fetchMember(msg.author, true)
-        guilds.push(guild)
-      } catch (ex) {
-      }
-    }
-    if (guilds.length === 1) {
-      server = conf.serversById[guilds[0].id]
-    }
-  }
-  if (!server) {
-    await sendDM(msg.author, 'We were unable to determine exactly one server associated with you and this bot, the DM interface will be limited.')
-  }
-
-  // The callback defined on each `.command` will be run if appropriate by
-  // yargs.
-  const yargs = require('yargs')()
-    .scriptName('')
-    .usage('')
-    .wrap(null)
-    .exitProcess(false)
-    .hide('version')
-    .hide('help')
-    .command('ping', 'check to see if the bot is alive', () => {}, argv => {
-      const [, ...args] = argv._
-      msg.reply(`I am alive` 
-        + (server ? `\nYour server is ${name(server)}` : '')
-        + (args.length ? '\nYour args were: ' + args.join('/') : ''))
-    })
-    .hide('ping')
-  if (server) yargs
-    .command('report', 'report inappropriate activity', () => {}, async argv => {
-      const [, ...report] = argv._
-      console.log(`**REPORT** from ${name(msg.author)} in ${name(msg.channel) || 'DM'}: ${report.join(' ')}`)
-      if (msg.channel.name) msg.delete().catch(() => {})
-      await server.moderation.send(`@here **REPORT** from ${msg.author} in ${msg.channel || 'DM'}: ${report.join(' ')}`)
-      sendDM(msg.author, `Report in ${msg.channel || 'DM'} has been received: ${report.join(' ')}`).catch(() => {})
-    })
-    .command('admin', 'message the admins', () => {}, async argv => {
-      const [, ...message] = argv._
-      console.log(`Admin message from ${name(msg.author)} in ${name(msg.channel) || 'DM'}: ${message.join(' ')}`)
-      if (msg.channel.name) msg.delete().catch(() => {})
-      await server.moderation.send(`Admin message from ${msg.author} in ${msg.cahnnel || 'DM'}: ${message.join(' ')}`)
-      sendDM(msg.author, `Message to admins sent from ${msg.channel || 'DM'}: ${message.join(' ')}`).catch(() => {})
-    })
-  if (msg.channel.name == null) yargs
-    .demand(1)
-    .recommendCommands()
-    .showHelpOnFail(true)
-    .help()
-
-  // yargs.parse is how we get yargs to read the string we have instead of
-  // our actual commandline.
-  return yargs.parse(cmd, function (err, argv, output) {
-    if (output) msg.reply(output)
-  })
-}
+// Utility functions
 
 function name (thing) {
    if (!thing) return thing
+   if (typeof thing === 'string') return thing
    let name = thing.name || thing.username
    if (thing.discriminator) name += '#' + thing.discriminator
    return name
 }
 
-async function clientGuildMemberAdd (gm) {
-  const server = conf.serversById[gm.guild.id]
-  await server.welcome.send(`Welcome to the server ${gm.user}! We are _so_ happy to see you! :heartpulse: Please check out the pinned message in this channel for the why's and whatfors of this server, plus our code of conduct!`)
+async function sendDM (user, msg) {
+  const dm = user.dmChannel || user.createDM()
+  return dm.send(msg)
 }
