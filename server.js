0613b9ac05b3c6d9a29376c62a8969d5d1500bbc
Rebecca Turner <me@re-becca.org>
2018-10-06 19:30:03 -0700

    Add comments

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict'
 require('@iarna/cli')(main)
+// These are yargs config for what goes on the commandline.
+  .usage('$0 <conffile>')
   .demand(1)
   .help()
 
@@ -17,22 +19,37 @@ const DEFAULTS = {
 let conf
 let client
 
+// @iarna/cli above will run this when the process starts
 async function main (opts, conffile) {
   conf = Object.assign({}, DEFAULTS, TOML.parse(fs.readFileSync(conffile)))
+
   client = new Discord.Client()
-    .on('ready', clientReady)
-    .on('error', clientError)
-    .on('message', clientMessage)
-    .on('messageReactionAdd', clientMessageReactionAdd)
-    .on('guildMemberAdd', clientGuildMemberAdd)
+    .on('ready',  eventRun(clientReady))
+    .on('error',  eventRun(clientError))
+    .on('message',  eventRun(clientMessage))
+    .on('messageReactionAdd',  eventRun(clientMessageReactionAdd))
+    .on('guildMemberAdd',  eventRun(clientGuildMemberAdd))
 
   client.login(conf.token)
 }
 
+// this little wrapper exists so that our async event handlers don't throw
+// away errors
+function eventRun (fn) {
+  return async (...args) => {
+    try {
+      return await fn(...args)
+    } catch (ex) {
+      console.log(`Error running ${fn.name}:`, ex)
+    }
+  }
+}
+
+// called at startup, also after reconnects
 async function clientReady () {
   console.log(`Logged in as ${client.user.tag}!`);
   client.guilds.forEach(guild => {
-  console.log(`Logged into ${guild.name}, ${guild.id}`);
+    console.log(`Logged into ${guild.name}, ${guild.id}`);
     if (conf.servers[guild.name]) {
       const server = conf.servers[guild.name]
       conf.serversById[guild.id] = server
@@ -45,6 +62,9 @@ async function clientReady () {
         }
       })
     } else {
+      // This would only happen if the bot were added to another server and
+      // I didn't have them in my config file. As bot DM functionality kinda breaks
+      // with multiple servers, I may not ever do that.
       console.log(`Unknown server: ${guild.name}`)
       conf.serversById[guild.id] = conf.servers[guild.name] = {
         name: guild.name,
@@ -70,25 +90,27 @@ async function clientMessageReactionAdd (mr, user) {
   dm.send(`Report in ${mr.message.channel || 'DM'} has been received: ${report}`).catch(() => {})
 }
 
-async function clientMessage (msg) {
-  if (msg.author.id === conf.id) return
-  try {  
-//    console.log('clientMessage', msg.channel.name, msg.author.id, msg.author.username, msg.author.discriminator, msg.content)
-    if (qr`^/\w`.test(msg)) {
-      return await runCommand(msg, msg.content.trim().slice(1))
-    } else if (msg.channel.name == null) {
-      return await runCommand(msg, msg.content.trim())
-    }
-  } catch (ex) {
-    console.log(ex)
-  }
+async function sendDM (user, msg) {
+  const dm = user.dmChannel || user.createDM()
+  dm.send(msg)
 }
 
-async function runCommand (msg, cmd, showHelp) {
+async function clientMessage (msg) {
+  if (msg.author.id === client.user.id) return // ignore our own messages
+  // Helpful when debugging, but logs ALL messages on the discord
+  //console.log('clientMessage', msg.channel.name, msg.author.id, msg.author.username, msg.author.discriminator, msg.content)
+  let cmd = msg.content.trim()
+  if (qr`^/\w`.test(cmd)) {
+    cmd = cmd.slice(1)
+  }
+
   let server
   if (msg.guild) {
     server = conf.serversById[msg.guild.id]
   } else {
+    // the msg is a DM, so we have find the user's server on our own, this
+    // is why we can't have nice things re: having this bot on multiple
+    // servers
     const guilds = []
     for (let [, guild] of client.guilds) {
       try {
@@ -102,8 +124,11 @@ async function runCommand (msg, cmd, showHelp) {
     }
   }
   if (!server) {
-    msg.author.dmChannel.send('We were unable to determine exactly one server associated with you and this bot, the DM interface will be limited.')
+    await sendDM(msg.author, 'We were unable to determine exactly one server associated with you and this bot, the DM interface will be limited.')
   }
+
+  // The callback defined on each `.command` will be run if appropriate by
+  // yargs.
   const yargs = require('yargs')()
     .scriptName('')
     .usage('')
@@ -111,29 +136,27 @@ async function runCommand (msg, cmd, showHelp) {
     .exitProcess(false)
     .hide('version')
     .hide('help')
-    .hide('ping')
-     .command('ping', 'check to see if the bot is alive', () => {}, argv => {    
+    .command('ping', 'check to see if the bot is alive', () => {}, argv => {
       const [, ...args] = argv._
       msg.reply(`I am alive` 
         + (server ? `\nYour server is ${name(server)}` : '')
         + (args.length ? '\nYour args were: ' + args.join('/') : ''))
     })
+    .hide('ping')
   if (server) yargs
     .command('report', 'report inappropriate activity', () => {}, async argv => {
       const [, ...report] = argv._
       console.log(`**REPORT** from ${name(msg.author)} in ${name(msg.channel) || 'DM'}: ${report.join(' ')}`)
       if (msg.channel.name) msg.delete().catch(() => {})
       await server.moderation.send(`@here **REPORT** from ${msg.author} in ${msg.channel || 'DM'}: ${report.join(' ')}`)
-      const dm = msg.author.dmChannel || await msg.author.createDM()
-      dm.send(`Report in ${msg.channel || 'DM'} has been received: ${report.join(' ')}`).catch(() => {})
+      sendDM(msg.author, `Report in ${msg.channel || 'DM'} has been received: ${report.join(' ')}`).catch(() => {})
     })
     .command('admin', 'message the admins', () => {}, async argv => {
       const [, ...message] = argv._
       console.log(`Admin message from ${name(msg.author)} in ${name(msg.channel) || 'DM'}: ${message.join(' ')}`)
       if (msg.channel.name) msg.delete().catch(() => {})
       await server.moderation.send(`Admin message from ${msg.author} in ${msg.cahnnel || 'DM'}: ${message.join(' ')}`)
-      const dm = msg.author.dmChannel || await msg.author.createDM()
-      dm.send(`Message to admins sent from ${msg.channel || 'DM'}: ${message.join(' ')}`).catch(() => {})
+      sendDM(msg.author, `Message to admins sent from ${msg.channel || 'DM'}: ${message.join(' ')}`).catch(() => {})
     })
   if (msg.channel.name == null) yargs
     .demand(1)
@@ -141,11 +164,11 @@ async function runCommand (msg, cmd, showHelp) {
     .showHelpOnFail(true)
     .help()
 
-  yargs
-    .parse(cmd, function (err, argv, output) {
-      if (output) msg.reply(output)
-    })
-    .argv
+  // yargs.parse is how we get yargs to read the string we have instead of
+  // our actual commandline.
+  return yargs.parse(cmd, function (err, argv, output) {
+    if (output) msg.reply(output)
+  })
 }
 
 function name (thing) {
